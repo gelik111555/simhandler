3a5825248311f0394f8623967f6a17bc18d88b10
gbook <gelik111555@gmail.com>
2024-01-09 06:37:30 +0300

    feat: Created HandlerBase class
    Added the abstract base class "HandlerBase" with a "Handle" method and three protected virtual methods for handling entry and exit.

---
+++ Application/Common/Handlers/HandlerBase.cs
@@ -0,0 +1,25 @@
+﻿//using Application.Common.Models;
+
+//namespace Application.Common.Handlers;
+
+//public abstract class HandlerBase
+//{
+//    public void Handle(ModemContext request)
+//    {
+//        OnHandleEntry(request);
+//        PerformHandling(request);
+//        OnHandleExit(request);
+//    }
+
+//    protected abstract void PerformHandling(ModemContext request);
+
+//    protected virtual void OnHandleEntry(ModemContext request)
+//    {
+//        // Действия перед основной обработкой
+//    }
+
+//    protected virtual void OnHandleExit(ModemContext request)
+//    {
+//        // Действия после основной обработки
+//    }
+//}
