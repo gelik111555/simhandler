a3cb8d316ce2bcd49a16c97d04cd7d47b2a198d1
gbook <gelik111555@gmail.com>
2024-01-09 06:39:23 +0300

    feat: Created ModemContext class

---
+++ Application/Common/Models/ModemContext.cs
@@ -0,0 +1,9 @@
+﻿//using ModemLib.Models.Modems;
+
+//namespace Application.Common.Models;
+
+//public class ModemContext
+//{
+//    public ModemBase ModemBase { get; set; }
+//    public CancellationTokenSource CancellationTokenSource { get; set; }
+//}
