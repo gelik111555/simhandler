8e3b9b3dad89d0db739c30042d69fb51f4ea6aaa
gbook <gelik111555@gmail.com>
2024-01-09 06:38:18 +0300

    feat: Created OperatorSettings class
    Defined the "OperatorSettings" class with properties for operator-specific settings.

---
+++ Application/Common/Models/Settings/Operator/OperatorSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Common.Models.Settings.Operator;
+
+public class OperatorSettings
+{
+    public string OperatorName { get; set; }
+    public string GetPhoneNumberUSSD { get; set; }
+    public int ActivationUSSD { get; set; }
+    public bool GetPhoneWithUSSDCodeOrSMS { get; set; }
+}
