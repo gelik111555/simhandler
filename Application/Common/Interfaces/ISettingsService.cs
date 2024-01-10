7689899692d4a39110ea125e9e9ff428aeda7a65
da4a <gelik111555@gmail.com>
2024-01-10 11:50:34 +0300

    Commit: Refactor SetOrUpdateSettingsForOperator parameter type
    Description: Updated the parameter type of the SetOrUpdateSettingsForOperator method in the ISettingsService interface to expect an IEnumerable<OperatorSettings> instead of ICollection<OperatorSettings> for improved flexibility.

--- Application/Common/Interfaces/ISettingsService.cs
+++ Application/Common/Interfaces/ISettingsService.cs
@@ -5,6 +5,6 @@ namespace Application.Common.Interfaces;
 public interface ISettingsService
 {
     public event EventHandler OperatorSettingsChanged;
-    public Task<OperatorSettings> GetSettingsForOperator(string name);
+    public Task<OperatorSettings?> GetSettingsForOperator(string name);
     public Task SetOrUpdateSettingsForOperator(ICollection<OperatorSettings> operatorSettings);
 }
