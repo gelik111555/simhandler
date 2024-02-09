1bf505294e138138b570ea7972a9e8c2bb280664
da4a <gelik111555@gmail.com>
2024-02-09 09:30:03 +0300

    feat: Introduce ModemService for streamlined modem interactions
    Introduced ModemService within the infrastructure layer, implementing the IModemService interface, to streamline interactions with modem hardware. This service encapsulates the logic for retrieving modem names and asynchronously obtaining signal levels, abstracting the complexities of direct modem communication. By integrating IModemBase for modem identification and IModemSignal for signal processing, ModemService provides a unified interface for modem operations, enhancing the application's capability to interact with modem hardware efficiently. This approach simplifies modem management tasks within the application, promoting clean code practices and improving overall system reliability.

---
+++ src/Infrastructure/ModemCommunication/ModemService.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Common.Interfaces;
+using ModemLib.Domain.Interfaces;
+
+namespace Infrastructure.ModemCommunication;
+
+public class ModemService : IModemService
+{
+    private readonly IModemSignal _signal;
+    private readonly CancellationToken _cancellationToken;
+    private IModemBase _modemBase;
+    internal ModemService(
+        IModemBase modemBase,
+        IModemSignal signal,
+        CancellationToken cancellationToken)
+    {
+        _modemBase = modemBase;
+        _signal = signal ?? throw new ArgumentNullException(nameof(signal));
+        _cancellationToken = cancellationToken;
+    }
+
+    public string GetName()
+    {
+        return _modemBase.ModemModel;
+    }
+
+    public async Task<string> GetSignalLevelAsync()
+    {
+        return await _signal.GetSignalAsync(_modemBase, _cancellationToken);
+    }
+}
