90c0175b5d1c162c0396e9dd3602ecf65996a1b5
da4a <gelik111555@gmail.com>
2024-02-09 09:32:45 +0300

    feat: Implement ComPort class for COM port enumeration

---
+++ src/Infrastructure/Common/System/ComPort.cs
@@ -0,0 +1,11 @@
+﻿using Application.Common.Interfaces;
+
+namespace Infrastructure.Common.System;
+
+public class ComPort : IComPort
+{
+    public IEnumerable<string> GetComPorts()
+    {
+        return ModemLib.Services.ComPortService.GetAllPorts();
+    }
+}
