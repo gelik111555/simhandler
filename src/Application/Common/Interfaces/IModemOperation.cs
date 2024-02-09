f08ef06f682ad928d1706ffb29a7243029401346
da4a <gelik111555@gmail.com>
2024-02-09 09:21:39 +0300

    feat: Establish IModemOperation for chained modem tasks
    Defined IModemOperation interface to support a chain of responsibility pattern for modem operations within the application. This allows for a sequence of modem tasks to be linked and handled asynchronously, enabling more flexible and maintainable modem interaction workflows.

---
+++ src/Application/Common/Interfaces/IModemOperation.cs
@@ -0,0 +1,9 @@
+﻿using Application.ModemProcessing.ModemOperation;
+
+namespace Application.Common.Interfaces;
+
+internal interface IModemOperation
+{
+    IModemOperation SetNext(IModemOperation modemOperation);
+    Task HandleAsync(ContextModemOperation modemOperation);
+}
