8603235ff5092e4a8f3f572d8e38182103f57968
da4a <gelik111555@gmail.com>
2024-02-09 09:24:00 +0300

    feat: Add LoggerForUserInterface to integrate with modem views

---
+++ src/Application/Common/Models/LoggerForUserInterface.cs
@@ -0,0 +1,22 @@
+﻿using Application.Common.Interfaces;
+
+namespace Application.Common.Models;
+
+public class LoggerForUserInterface : ILoggerForUserInterface
+{
+    private readonly IModemViewDataManager _comPortViewManager;
+
+    public LoggerForUserInterface(IModemViewDataManager comPortViewManager)
+    {
+        _comPortViewManager = comPortViewManager;
+    }
+    public void AddMessage(string comPort, string message)
+    {
+        _comPortViewManager.GetComPortView(comPort).ViewLogger.Add(message);
+    }
+
+    public void ClearLog(string comPort)
+    {
+        _comPortViewManager.GetComPortView(comPort).ViewLogger.Clear();
+    }
+}
