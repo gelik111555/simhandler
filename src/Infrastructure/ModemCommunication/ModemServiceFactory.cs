6f305b282125b7d977af185bf9a22a0b5f860d73
da4a <gelik111555@gmail.com>
2024-02-09 09:27:46 +0300

    feat: Implement ModemServiceFactory for dynamic modem service creation
    Implemented the ModemServiceFactory to dynamically create instances of IModemService based on provided COM port identifiers. This factory pattern enhances the application's flexibility by encapsulating the logic for initializing modem services, including acquiring the modem base and setting up the modem signal. By injecting dependencies such as ILogger, CancellationToken, and IModem, the factory ensures that each modem service is properly configured with the necessary context and resources for its operation. This approach promotes a clean separation of concerns and makes the modem communication layer more adaptable to changes in the underlying modem infrastructure.

---
+++ src/Infrastructure/ModemCommunication/ModemServiceFactory.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Common.Interfaces;
+using Microsoft.Extensions.Logging;
+using ModemLib.Domain.Interfaces;
+
+namespace Infrastructure.ModemCommunication;
+
+public class ModemServiceFactory : IModemServiceFactory
+{
+    private readonly ILogger<ModemServiceFactory> _logger;
+    private readonly CancellationToken _cancellationToken;
+    private readonly IModem _modem;
+     
+    public ModemServiceFactory(
+        ILogger<ModemServiceFactory> logger,
+        CancellationToken cancellationToken,
+        IModem modem)
+    {
+        _logger = logger;
+        _cancellationToken = cancellationToken;
+        _modem = modem;
+    }
+    public async Task<IModemService> CreateAsync(string comPort)
+    {
+        IModemSignal signal = new ModemSignal();
+        IModemBase modemBase = await _modem.GetModemAsync(comPort, _logger, _cancellationToken);
+        return new ModemService(modemBase, signal, _cancellationToken);
+    }
+}
