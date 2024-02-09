0ace6aec82593203824dd7b24e6142fb6e9694f0
da4a <gelik111555@gmail.com>
2024-02-09 09:28:39 +0300

    feat: Enhance ModemSignal with signal extraction and conversion
    Enhanced ModemSignal within the infrastructure layer to include advanced signal processing capabilities. This update introduces a robust mechanism for extracting signal level data from modem responses using regular expressions, specifically targeting the signal strength format. It throws custom exceptions for scenarios where the modem response is empty or doesn't contain valid signal information, improving error handling and reliability. The conversion method translates raw signal quality numbers into a decibel format, providing a more standardized measure of signal strength. This enhancement ensures that the application can accurately interpret and utilize modem signal information, contributing to more effective communication management.

---
+++ src/Infrastructure/ModemCommunication/ModemSignal.cs
@@ -0,0 +1,47 @@
+﻿using Application.Common.Exceptions;
+using Infrastructure.Common.Interfaces;
+using ModemLib.Domain.Interfaces;
+using System.Text.RegularExpressions;
+
+namespace Infrastructure.ModemCommunication;
+
+internal partial class ModemSignal : IModemSignal
+{
+    private string _responseModem;
+    private const string _errorEmptyMessage = "Message with signal level was empty";
+    private const string _errorResponseMessage = "It is not possible to get numbers from the modem's response to determine the signal strength";
+    private const string _regularExpressionForSearchingForNumbers = @"\b\d{2},\d{2}\b";
+    private float _signalQuality;
+
+    public async Task<string> GetSignalAsync(IModemBase modem, CancellationToken cancellationToken)
+    {
+        var message = await modem.GetSignalLevelAsync(cancellationToken);
+        if (message.MessageIsEmpty) 
+            throw new EmptyAnswerFromModemException(_errorEmptyMessage);
+        _responseModem = message.Message;
+        return ExtractSignalLevelInDecibels();
+    }
+
+    private string ExtractSignalLevelInDecibels()
+    {
+        GetOnlyDigits();
+        return ConversionToDecibelsFormat();
+    }
+
+    [GeneratedRegex("\\+CSQ: (\\d+,\\d+)")]
+    private static partial Regex SearchingForOnlyNumbers();
+    private void GetOnlyDigits()
+    {
+        var match = SearchingForOnlyNumbers().Match(_responseModem);
+        if (!match.Success)
+            throw new ModemResponseException(_errorResponseMessage);
+        _signalQuality = float.Parse(match.Groups[1].Value);
+    }
+
+    private string ConversionToDecibelsFormat()
+    {
+        return (_signalQuality * 2 - 113).ToString();
+    }
+
+    
+}
