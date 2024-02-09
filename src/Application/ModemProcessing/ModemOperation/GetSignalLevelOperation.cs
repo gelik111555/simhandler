7af75fccf2690b8ce791965bdffdf7865094ef9e
da4a <gelik111555@gmail.com>
2024-02-09 09:25:57 +0300

    feat: Add GetSignalLevelOperation for signal level retrieval

---
+++ src/Application/ModemProcessing/ModemOperation/GetSignalLevelOperation.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Application.ModemProcessing.ModemOperation;
+
+internal class GetSignalLevelOperation : BaseModemOperation
+{
+    protected override async Task HandleOperationAsync(ContextModemOperation modemOperation)
+    {
+        await modemOperation.ModemService.GetSignalLevelAsync();
+    }
+
+    protected override void LogAfter()
+    {
+        
+    }
+
+    protected override void LogBefore()
+    {
+       
+    }
+}
