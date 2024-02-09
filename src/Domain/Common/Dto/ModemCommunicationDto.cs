7a8f1313c2d6530072c84dbf09799a6f581bce58
da4a <gelik111555@gmail.com>
2024-02-09 09:31:52 +0300

    feat: Introduce ModemCommunicationDto for streamlined modem operations

---
+++ src/Domain/Common/Dto/ModemCommunicationDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Common.Dto;
+
+public class ModemCommunicationDto
+{
+    public string ComPort { get; }
+    public CancellationToken CancellationToken { get;}
+
+    public ModemCommunicationDto(string comPort, CancellationToken cancellationToken)
+    {
+        ComPort = comPort;
+        CancellationToken = cancellationToken;
+    }
+}
