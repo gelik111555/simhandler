a4af7681339cc3cd88f215beb807cc802592b005
da4a <gelik111555@gmail.com>
2024-02-09 09:23:04 +0300

    feat: Define ModemViewData model for modem information display
    Introduced ModemViewData class to encapsulate the details necessary for displaying modem information, including the COM port, name, signal strength, and a view logger for event tracking. This model facilitates the structured representation of modem data within the user interface, improving data coherence and interaction experience.

---
+++ src/Application/Common/Models/ModemView/ModemViewData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Application.Common.Models.ModemView;
+
+public class ModemViewData
+{
+    public ModemViewData(string comPort)
+    {
+        ComPort = comPort;
+    }
+    public string Name { get; set; }
+    public string Signal { get; set; }
+    public string ComPort { get; }
+    public ObservableCollection<string> ViewLogger { get; }
+
+}
