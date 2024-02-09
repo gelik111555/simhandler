20d88ebe7c8caf6ba2b16fb46902840c9c8878a0
da4a <gelik111555@gmail.com>
2024-02-09 09:25:42 +0300

    Added ContextModemOperation

---
+++ src/Application/ModemProcessing/ModemOperation/ContextModemOperation.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.Interfaces;
+
+namespace Application.ModemProcessing.ModemOperation;
+
+public class ContextModemOperation
+{
+    public ContextModemOperation(IModemService modemService)
+    {
+        ModemService = modemService;
+    }
+
+    public IModemService ModemService { get; }
+}
