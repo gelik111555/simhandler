3dd0ba41550917ddaffa905294d64393b6d0b590
da4a <gelik111555@gmail.com>
2024-02-09 09:22:43 +0300

    feat: Introduce IModemViewDataManager for modem view management

---
+++ src/Application/Common/Interfaces/IModemViewDataManager.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Models;
+using Application.Common.Models.ModemView;
+using System.Collections.ObjectModel;
+
+namespace Application.Common.Interfaces;
+
+public interface IModemViewDataManager
+{
+    void AddComPortsView(IEnumerable<string> comPorts);
+    ModemViewData GetComPortView(string comPort);
+    ObservableCollection<ModemViewData> GetAll();
+}
