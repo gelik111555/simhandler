f4d915b6d1d2f76d726b13e154afba5cd1ea2a9d
da4a <gelik111555@gmail.com>
2024-02-09 09:33:24 +0300

    feat: Develop MainDataGridHandler for modem management
    Implemented MainDataGridHandler within the application layer to orchestrate modem management tasks, including the enumeration of COM ports, modem initialization, and setup for continuous operation. Leveraging services such as IModemServiceFactory, ILogger, IModemViewDataManager, and IComPort, this handler encapsulates the logic required to dynamically manage modem configurations based on available COM ports. The handler supports both development and production environments by conditionally processing a predefined COM port or iterating over all detected ports. Additionally, it integrates with the ChainModemHandle for initiating modem operations, ensuring modems are ready for communication tasks. This comprehensive approach enhances the application's ability to manage modem resources efficiently, aligning with best practices for asynchronous operation and error management.

---
+++ src/Application/ModemProcessing/MainDataGridHandler.cs
@@ -0,0 +1,84 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using Application.Common.Models.ModemView;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Application.ModemProcessing;
+
+public class MainDataGridHandler : IMainDataGridHandler
+{
+    private readonly IModemServiceFactory _modemServiceFactory;
+    private readonly ILogger<MainDataGridHandler> _logger;
+    private readonly IModemViewDataManager _modemViewDataManager;
+    private readonly IHostApplicationLifetime _hostApplicationLifetime;
+    private readonly IComPort _comPort;
+    private IEnumerable<string> _comPorts;
+    private readonly bool _isDevelopment = true;
+    private readonly string _developmentComPort = "COM103";
+    private ICollection<Dictionary<ModemViewData, IModemService>> _modemViewAndService;
+
+    public MainDataGridHandler(IModemServiceFactory modemServiceFactory,
+        ILogger<MainDataGridHandler> logger,
+        IModemViewDataManager modemViewDataManager,
+        IHostApplicationLifetime hostApplicationLifetime,
+        IComPort comPort)
+    {
+        _modemServiceFactory = modemServiceFactory;
+        _logger = logger;
+        _modemViewDataManager = modemViewDataManager;
+        _hostApplicationLifetime = hostApplicationLifetime;
+        _comPort = comPort;
+    }
+    public async Task Handle()
+    {
+        try
+        {
+            GetComPorts();
+            FillingViewDataManager();
+            await StartUpModemsHandle();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogCritical(ex.Message);
+            throw;
+        }
+    }
+    private void GetComPorts()
+    {
+        _comPorts = _comPort.GetComPorts();
+    }
+    private void FillingViewDataManager()
+    {
+        _modemViewDataManager.AddComPortsView(_comPorts);
+    }
+
+    private async Task StartUpModemsHandle()
+    {
+        if (_isDevelopment)
+        {
+            await StartModem(_developmentComPort);
+        }
+        else
+        {
+            foreach (var comPort in _comPorts)
+            {
+                await StartModem(comPort);
+            }
+        }
+    }
+
+    private async Task StartModem(string comPort)
+    {
+        await Task.Run(async () =>
+        {
+            var modemService = await CreateModemServices(comPort);
+            ChainModemHandle chainModemHandle = new(modemService, _hostApplicationLifetime);
+            await chainModemHandle.StartUp();
+        });
+    }
+    private async Task<IModemService> CreateModemServices(string comPort)
+    {
+        return await _modemServiceFactory.CreateAsync(comPort);
+    }
+}
