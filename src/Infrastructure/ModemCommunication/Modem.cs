010ee5e2e85ce57dfb53a6e4fa788b1e469ee3a3
da4a <gelik111555@gmail.com>
2024-02-09 09:29:34 +0300

    feat: Implement Modem class for asynchronous modem instantiation

---
+++ src/Infrastructure/ModemCommunication/Modem.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Common.Interfaces;
+using Microsoft.Extensions.Logging;
+using ModemLib.Domain.Interfaces;
+using ModemLib.Factories.Modem;
+using ModemLib.Models;
+namespace Infrastructure.ModemCommunication;
+
+public class Modem: IModem
+{
+    public async Task<IModemBase> GetModemAsync(string comPort, ILogger logger, CancellationToken cancellationToken)
+    {
+        ModemConfigurator modemConfigurator = new(comPort, logger, cancellationToken);
+        return await new ModemFactory().CreateAsync(modemConfigurator);
+    }
+}
