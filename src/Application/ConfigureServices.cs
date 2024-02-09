68266f682f80aa2dce246d9d30291eefdb6f5606
da4a <gelik111555@gmail.com>
2024-02-09 09:27:04 +0300

    feat: Configure application services for dependency injection
    Implemented ConfigureServices extension method to facilitate the configuration of application-specific services within the dependency injection container. This method enhances the application's modularity and testability by registering services such as IModemViewDataManager and IMainDataGridHandler with their concrete implementations. By leveraging the IServiceCollection and IConfiguration interfaces, this setup allows for a centralized and flexible configuration of services, promoting loose coupling and making the application easier to maintain and extend.

---
+++ src/Application/ConfigureServices.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Interfaces;
+using Application.ModemProcessing;
+using Microsoft.Extensions.Configuration;
+using Application.Common.Models.ModemView;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<IModemViewDataManager, ModemViewDataManager>();
+        services.AddScoped<IMainDataGridHandler, MainDataGridHandler>();
+        return services;
+    }
+}
