03247ebb33cdeb663e783b12b85eaf5e3060d6ce
da4a <gelik111555@gmail.com>
2024-02-09 09:25:26 +0300

    feat: Implement BaseModemOperation for modular modem processing
    Implemented BaseModemOperation as an abstract class that forms the foundation of a chain of responsibility pattern for handling modem operations. This class provides a template method for executing a sequence of modem operations, including pre and post-operation logging. It supports linking operations together, allowing for flexible and extensible handling of modem interactions in a decoupled manner. This architecture facilitates adding new modem operations without altering existing code, enhancing maintainability and scalability.

---
+++ src/Application/ModemProcessing/ModemOperation/BaseModemOperation.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.Interfaces;
+
+namespace Application.ModemProcessing.ModemOperation;
+
+internal abstract class BaseModemOperation : IModemOperation
+{
+    private IModemOperation _next;
+    public async Task HandleAsync(ContextModemOperation modemOperationContext)
+    {
+        LogBefore();
+        await HandleOperationAsync(modemOperationContext);
+        LogAfter();
+        if (_next != null) await _next.HandleAsync(modemOperationContext);
+    }
+
+    public IModemOperation SetNext(IModemOperation modemOperation)
+    {
+        _next = modemOperation;
+        return modemOperation;
+    }
+    protected abstract Task HandleOperationAsync(ContextModemOperation modemOperation);
+    protected abstract void LogBefore();
+    protected abstract void LogAfter();
+}
