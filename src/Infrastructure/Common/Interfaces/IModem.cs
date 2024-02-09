a8bbc46f82a61ca0efc49c9d27fcce51d7843a8f
da4a <gelik111555@gmail.com>
2024-02-09 09:31:09 +0300

    feat: Add EventInfoId for structured logging of modem errors

---
+++ src/Infrastructure/Common/Interfaces/IModem.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Logging;
+using ModemLib.Domain.Interfaces;
+
+namespace Infrastructure.Common.Interfaces;
+
+public interface IModem
+{
+    public Task<IModemBase> GetModemAsync(string comPort, ILogger logger, CancellationToken cancellationToken);
+}
