9be8e0e9f9fa85dbdf88f6f21e2e54eafe77af2d
da4a <gelik111555@gmail.com>
2024-02-09 09:19:28 +0300

    feat: Implement ModemResponseException for error handling
    Introduced ModemResponseException to encapsulate errors related to modem response processing. This custom exception will improve error management and debugging by providing more context when modem interactions fail.

---
+++ src/Application/Common/Exceptions/ModemResponseException.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Common.Exceptions;
+
+public class ModemResponseException : Exception
+{
+    public ModemResponseException(string message) : base(message) { }
+    public ModemResponseException(string message, Exception innerException)
+       : base(message, innerException)
+    {
+    }
+}
