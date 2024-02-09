876c41a032a763640c6d1bbb1fa08db657bff6d2
da4a <gelik111555@gmail.com>
2024-02-09 09:32:25 +0300

    feat: Define IModemSignal interface for signal retrieval

---
+++ src/Infrastructure/Common/Interfaces/IModemSignal.cs
@@ -0,0 +1,9 @@
+﻿using ModemLib.Domain.Interfaces;
+using ModemLib.Models.Modems;
+
+namespace Infrastructure.Common.Interfaces;
+
+internal interface IModemSignal
+{
+    public Task<string> GetSignalAsync(IModemBase modem, CancellationToken cancellationToken);
+}
