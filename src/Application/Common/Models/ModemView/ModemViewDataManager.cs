5ef58a030b77ed6b2ebbd3d4c5a29561c1a25d06
da4a <gelik111555@gmail.com>
2024-02-09 09:23:42 +0300

    feat: Implement ModemViewDataManager for centralized view management
    Implemented ModemViewDataManager to act as a central repository for managing modem views within the application. It provides functionalities to add modem views for COM ports, retrieve specific modem view data, and get a collection of all modem views. This implementation enhances the application's ability to dynamically manage and display modem information, facilitating a more interactive and user-friendly interface.

---
+++ src/Application/Common/Models/ModemView/ModemViewDataManager.cs
@@ -0,0 +1,32 @@
+﻿using Application.Common.Interfaces;
+using System.Collections.ObjectModel;
+
+namespace Application.Common.Models.ModemView;
+
+public class ModemViewDataManager : IModemViewDataManager
+{
+    private readonly Dictionary<string, ModemViewData> _comPortViews = new();
+
+    public void AddComPortsView(IEnumerable<string> comPorts)
+    {
+        foreach (var item in comPorts)
+        {
+            _comPortViews[item] = new ModemViewData(item);
+        }
+    }
+
+    public ModemViewData GetComPortView(string comPort)
+    {
+        if (_comPortViews.TryGetValue(comPort, out ModemViewData view))
+        {
+            return view;
+        }
+
+        throw new KeyNotFoundException($"View for COM port '{comPort}' not found.");
+    }
+
+    public ObservableCollection<ModemViewData> GetAll()
+    {
+        return new ObservableCollection<ModemViewData>(_comPortViews.Values);
+    }
+}
