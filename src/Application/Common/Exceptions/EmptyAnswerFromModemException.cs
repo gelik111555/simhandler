09f4cbef5aa8d4a8106d13ffdb057b202dadfaa3
da4a <gelik111555@gmail.com>
2024-02-09 09:18:56 +0300

    feat: Add EmptyAnswerFromModemException handling
    Added a custom exception class EmptyAnswerFromModemException to handle cases where the modem response is empty or invalid. This ensures that the application can gracefully handle unexpected or missing data from modem communications.

---
+++ src/Application/Common/Exceptions/EmptyAnswerFromModemException.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.Common.Exceptions;
+
+public class EmptyAnswerFromModemException : Exception
+{
+    public EmptyAnswerFromModemException()
+        : base()
+    {
+    }
+
+    public EmptyAnswerFromModemException(string message)
+        : base(message)
+    {
+    }
+
+    public EmptyAnswerFromModemException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+
+}
