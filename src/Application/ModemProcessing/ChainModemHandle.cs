f1f4645422d6fc73566d547e8c4599448eedf33f
da4a <gelik111555@gmail.com>
2024-02-09 09:26:25 +0300

    feat: Implement ChainModemHandle for operation sequencing
    Implemented ChainModemHandle to orchestrate a sequence of modem operations using a chain of responsibility pattern. This class initializes with a modem service and application lifetime, setting up a chain of modem operations to be executed during the application's runtime. It systematically processes each operation in the chain, handling exceptions gracefully to ensure continuous operation. The setup includes adding specific modem operations to a list and creating a linked chain to facilitate sequential execution. This approach allows for dynamic handling of modem operations based on runtime conditions, enhancing the application's flexibility and resilience.

---
+++ src/Application/ModemProcessing/ChainModemHandle.cs
@@ -0,0 +1,56 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using Application.ModemProcessing.ModemOperation;
+using Microsoft.Extensions.Hosting;
+using System.Threading;
+
+namespace Application.ModemProcessing;
+
+internal class ChainModemHandle
+{
+    private readonly IModemService _modemService;
+    private readonly IHostApplicationLifetime _hostApplicationLifetime;
+    private List<IModemOperation> _modemOperations = new();
+    private IModemOperation _chainOperations;
+
+    internal ChainModemHandle(IModemService modemService, IHostApplicationLifetime  hostApplicationLifetime)
+    {
+        _modemService = modemService;
+        _hostApplicationLifetime = hostApplicationLifetime;
+        AddModemOperationToList();
+        CreateChainOperation();
+    }
+    internal async Task StartUp()
+    {
+        var context = new ContextModemOperation(_modemService);
+
+        while (!_hostApplicationLifetime.ApplicationStopping.IsCancellationRequested)
+        {
+            try
+            {
+                await _chainOperations.HandleAsync(context);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+
+
+    private void AddModemOperationToList()
+    {
+        _modemOperations.Add(new GetSignalLevelOperation());
+    }
+
+    private void CreateChainOperation()
+    {
+        IModemOperation previousOperation = null;
+        foreach (var operation in _modemOperations)
+        {
+            previousOperation?.SetNext(operation);
+            previousOperation = operation;
+        }
+        _chainOperations = previousOperation;
+    }
+}
