486e2f29889025dfddfc43af25ef728ba95b2b85
da4a <gelik111555@gmail.com>
2024-02-11 17:51:48 +0300

    feat: Introduce LogMessageWithKey DTO for logging
    This commit adds the LogMessageWithKey class within the Domain.Common.Dto namespace. It provides constructors for creating log message objects with optional messages and a mandatory comPort parameter. This class aims to structure logging information, especially for components interacting with COM ports, enhancing debug and log management.

---
+++ src/Domain/Common/Dto/LogMessageWithKey.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Common.Dto;
+
+
+//TODO: Need to delete
+public class LogMessageWithKey
+{
+    public LogMessageWithKey(string comPort)
+    {
+        ComPort = comPort;
+    }
+    public LogMessageWithKey(string message, string comPort)
+    {
+        Message = message;
+        ComPort = comPort;
+    }
+    public string? Message { get; }
+    public string ComPort { get; }
+}
