d9901e57a3c752479166d4792668542d9755bc92
da4a <gelik111555@gmail.com>
2024-02-11 17:50:53 +0300

    ModemContext deleted

--- src/Application/Common/Models/ModemContext.cs
+++
@@ -1,9 +0,0 @@
-﻿//using ModemLib.Models.Modems;
-
-//namespace Application.Common.Models;
-
-//public class ModemContext
-//{
-//    public ModemBase ModemBase { get; set; }
-//    public CancellationTokenSource CancellationTokenSource { get; set; }
-//}
