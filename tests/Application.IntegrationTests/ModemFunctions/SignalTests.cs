d0665030ffe82d95e23142acde7b646d12280b31
da4a <gelik111555@gmail.com>
2024-02-11 18:32:12 +0300

    fix: tests GetSignalAsync_ReturnsCorrectSignalLevel_WhenModemProvidesValidResponse

--- tests/Application.IntegrationTests/ModemFunctions/SignalTests.cs
+++ tests/Application.IntegrationTests/ModemFunctions/SignalTests.cs
@@ -58,33 +58,33 @@ public class SignalTests
         Assert.ThrowsAsync<ModemResponseException>(async () => await modemService.GetSignalLevelAsync());
     }
 
-    //[Test]
-    //public async Task GetSignalAsync_ReturnsCorrectSignalLevel_WhenModemProvidesValidResponse()
-    //{
-    //    // Arrange
-    //    var modemMock = new Mock<IModem>();
-    //    var modemBaseMock = new Mock<IModemBase>();
-    //    var loggerMock = new Mock<ILogger>();
-    //    var cancellationToken = new CancellationToken();
+    [Test]
+    public async Task GetSignalAsync_ReturnsCorrectSignalLevel_WhenModemProvidesValidResponse()
+    {
+        // Arrange
+        var modemMock = new Mock<IModem>();
+        var modemBaseMock = new Mock<IModemBase>();
+        var loggerMock = new Mock<ILogger>();
+        var cancellationToken = new CancellationToken();
 
-    //    string comPort = "COM103";
-    //    string expectedResponse = "+CSQ: 15,0\nOK"; // Имитация ожидаемого ответа от модема
-    //    var responseModem = new ResponseModem(expectedResponse);
+        string comPort = "COM103";
+        string expectedResponse = "+CSQ: 15,0\nOK"; // Имитация ожидаемого ответа от модема
+        var responseModem = new ResponseModem(expectedResponse);
 
-    //    modemBaseMock.Setup(m => m.GetSignalLevelAsync(cancellationToken))
-    //                 .ReturnsAsync(responseModem);
+        modemBaseMock.Setup(m => m.GetSignalLevelAsync(cancellationToken))
+                     .ReturnsAsync(responseModem);
 
-    //    modemMock.Setup(m => m.GetModemAsync(comPort, It.IsAny<ILogger>(), cancellationToken))
-    //             .ReturnsAsync(modemBaseMock.Object);
+        modemMock.Setup(m => m.GetModemAsync(comPort, It.IsAny<ILogger>(), cancellationToken))
+                 .ReturnsAsync(modemBaseMock.Object);
 
-    //    var modemService = new ModemService(modemBaseMock.Object, new ModemSignal(), cancellationToken);
+        var modemService = new ModemService(modemBaseMock.Object, new ModemSignal(), cancellationToken);
 
-    //    // Act
-    //    var result = await modemService.GetSignalLevelAsync();
+        // Act
+        var result = await modemService.GetSignalLevelAsync();
 
-    //    // Assert
-    //    Assert.AreEqual("-83", result);
-    //}
+        // Assert
+        Assert.AreEqual("-83", result);
+    }
 
 }
 
