cbf5a9019fbcf4756566ef7c419ad4e51e43f887
da4a <gelik111555@gmail.com>
2024-02-11 18:00:47 +0300

    refactor: Streamline Program class and integrate Serilog

--- src/WPF/Program.cs
+++ src/WPF/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using System;
+using Serilog;
 
 namespace WPF;
 
@@ -10,15 +11,25 @@ public static class Program
     public static void Main()
     {
         var app = new App();
-        app.InitializeComponent();
+        //app.InitializeComponent();
         app.Run();
     }
-    
-    public static IHostBuilder CreateHostBuilder(string[] Args) =>
-        Host.CreateDefaultBuilder(Args)
+
+    public static IHostBuilder CreateHostBuilder(string[] Args)
+    {
+        return Host.CreateDefaultBuilder(Args)
            .UseContentRoot(App.CurrentDirectory)
            .ConfigureAppConfiguration((host, cfg) => cfg
                .SetBasePath(App.CurrentDirectory)
                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true))
+            .UseSerilog((context, services, configuration) =>
+                {
+                    configuration
+                        .ReadFrom.Configuration(context.Configuration)
+                        .MinimumLevel.Information()
+                        .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+                        .WriteTo.Console();
+                })
            .ConfigureServices(App.ConfigureServices);
+    }
 }
